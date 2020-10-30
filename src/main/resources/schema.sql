7c9149200d853a8a952591496973f2679350fcb1
ClearJulyComes <viktordomogashev@gmail.com>
2020-10-31 01:40:58 +0300

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,14 @@
-CREATE TABLE IF NOT EXISTS Organisation (
+CREATE TABLE IF NOT EXISTS Organization (
     id          INTEGER                 COMMENT 'Уникальный идентификатор'      PRIMARY KEY AUTO_INCREMENT,
     name        VARCHAR(50) NOT NULL    COMMENT 'Название организации',
     full_name   VARCHAR(50) NOT NULL    COMMENT 'Полное название организации',
-    inn         INTEGER     NOT NULL    COMMENT 'ИНН организации',
-    kpp         INTEGER     NOT NULL    COMMENT 'КПП организации',
+    inn         VARCHAR(15) NOT NULL    COMMENT 'ИНН организации',
+    kpp         VARCHAR(15) NOT NULL    COMMENT 'КПП организации',
     address     VARCHAR(80) NOT NULL    COMMENT 'Адрес организации',
     phone       VARCHAR(20)             COMMENT 'Номер организации',
     is_active   BOOLEAN                 COMMENT 'Состояние организации'
 );
-COMMENT ON TABLE Organisation IS 'Организация';
+COMMENT ON TABLE Organization IS 'Организация';
 
 CREATE TABLE IF NOT EXISTS Office (
     id          INTEGER                 COMMENT 'Уникальный идентификатор'      PRIMARY KEY AUTO_INCREMENT,
@@ -19,11 +19,11 @@ CREATE TABLE IF NOT EXISTS Office (
 );
 COMMENT ON TABLE Office IS 'Офис';
 
-CREATE TABLE IF NOT EXISTS UserPosition (
+CREATE TABLE IF NOT EXISTS User_Position (
     id          INTEGER                         COMMENT 'Уникальный идентификатор'  PRIMARY KEY AUTO_INCREMENT,
     position    VARCHAR(20) NOT NULL    UNIQUE  COMMENT 'Название должности'
 );
-COMMENT ON TABLE UserPosition IS 'Должность';
+COMMENT ON TABLE User_Position IS 'Должность';
 
 CREATE TABLE IF NOT EXISTS Country (
     code    INTEGER               COMMENT 'Уникальный идентификатор'    PRIMARY KEY,
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS Country (
 );
 COMMENT ON TABLE Country IS 'Страна';
 
-CREATE TABLE IF NOT EXISTS UserInfo (
+CREATE TABLE IF NOT EXISTS User_Info (
     id                  INTEGER                 COMMENT 'Уникальный идентификатор'  PRIMARY KEY AUTO_INCREMENT,
     office_id           INTEGER     NOT NULL    COMMENT 'Индентификатор офиса',
     first_name          VARCHAR(50) NOT NULL    COMMENT 'Имя пользователя',
@@ -42,7 +42,7 @@ CREATE TABLE IF NOT EXISTS UserInfo (
     citizenship_code    INTEGER                 COMMENT 'Код страны',
     is_identified       BOOLEAN                 COMMENT 'Состояние человека'
 );
-COMMENT ON TABLE UserInfo IS 'Пользователь';
+COMMENT ON TABLE User_Info IS 'Пользователь';
 
 CREATE TABLE IF NOT EXISTS Doc (
     code    INTEGER                      COMMENT 'Код документа'        PRIMARY KEY,
@@ -60,20 +60,20 @@ CREATE TABLE IF NOT EXISTS User_Doc (
 );
 COMMENT ON TABLE User_Doc IS 'Документ пользователя';
 
-CREATE INDEX IX_Organization_Is_Active ON Organisation (is_active);
+CREATE INDEX IX_Organization_Is_Active ON Organization (is_active);
 
 CREATE INDEX IX_Office_Is_Active ON Office (is_active);
 CREATE INDEX IX_Office_Org_Id ON Office (org_id);
-ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organisation (id);
+ALTER TABLE Office ADD FOREIGN KEY (org_id) REFERENCES Organization (id);
 
-CREATE INDEX IX_UserInfo_Office_Id ON UserInfo (office_id);
-CREATE INDEX IX_UserInfo_Position_Id ON UserInfo (position_id);
-CREATE INDEX IX_UserInfo_Citizenship_Code ON UserInfo (citizenship_code);
-ALTER TABLE UserInfo ADD FOREIGN KEY (office_id) REFERENCES Office (id);
-ALTER TABLE UserInfo ADD FOREIGN KEY (position_id) REFERENCES UserPosition (id);
-ALTER TABLE UserInfo ADD FOREIGN KEY (citizenship_code) REFERENCES Country (code);
+CREATE INDEX IX_User_Info_Office_Id ON User_Info (office_id);
+CREATE INDEX IX_User_Info_Position_Id ON User_Info (position_id);
+CREATE INDEX IX_User_Info_Citizenship_Code ON User_Info (citizenship_code);
+ALTER TABLE User_Info ADD FOREIGN KEY (office_id) REFERENCES Office (id);
+ALTER TABLE User_Info ADD FOREIGN KEY (position_id) REFERENCES User_Position (id);
+ALTER TABLE User_Info ADD FOREIGN KEY (citizenship_code) REFERENCES Country (code);
 
 CREATE INDEX IX_User_Doc_Doc_Name ON User_Doc (doc_name);
 CREATE UNIQUE INDEX UX_User_Doc_User_Id ON User_Doc (user_id);
 ALTER TABLE User_Doc ADD FOREIGN KEY (doc_name) REFERENCES Doc (name);
-ALTER TABLE User_Doc ADD FOREIGN KEY (user_id) REFERENCES UserInfo (id);
+ALTER TABLE User_Doc ADD FOREIGN KEY (user_id) REFERENCES User_Info (id);
